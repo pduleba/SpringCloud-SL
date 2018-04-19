07bd43681c54c9bf748c8e20348f3f85a9e361b8
pduleba <pduleba@pgs-soft.com>
2018-04-19 09:27:36 +0200

    Chapter 1 :: Descovery Server

---
+++ discovery-server/src/main/java/pd/pduleba/spring/cloud/sl/discoveryserver/DiscoveryServerApplication.java
@@ -0,0 +1,12 @@
+package pd.pduleba.spring.cloud.sl.discoveryserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoveryServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryServerApplication.class, args);
+	}
+}
