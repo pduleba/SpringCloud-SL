2fe67a5e7611aa886d8f972013ee2f4ce83416be
pduleba <pduleba@pgs-soft.com>
2018-04-19 15:14:18 +0200

    Chapter 1 :: Food Service Client

---
+++ food-service-client/src/main/java/pd/pduleba/spring/cloud/sl/FoodServiceClientApplication.java
@@ -0,0 +1,12 @@
+package pd.pduleba.spring.cloud.sl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodServiceClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodServiceClientApplication.class, args);
+	}
+}
