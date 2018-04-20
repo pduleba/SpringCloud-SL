438b5451e8130831b42c5e1e9b8ebd2c532f13c2
pduleba <pduleba@pgs-soft.com>
2018-04-20 08:27:42 +0200

    Chapter 2 :: Food Service Configuration Server :: Setup

--- food-service-config/src/main/java/pd/pduleba/spring/cloud/sl/foodserviceconfig/FoodServiceConfigApplication.java
+++ food-service-config/src/main/java/pd/pduleba/spring/cloud/sl/foodserviceconfig/FoodServiceConfigApplication.java
@@ -2,7 +2,9 @@ package pd.pduleba.spring.cloud.sl.foodserviceconfig;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class FoodServiceConfigApplication {
 
