f164810aed2ebbb11d6816394a13aada84c69170
pduleba <pduleba@pgs-soft.com>
2018-05-09 10:59:10 +0200

    Chapter 5 :: Hystrix Dashboard :: Init

---
+++ food-service-hystrix-dashboard/src/main/java/pd/pduleba/spring/cloud/sl/foodservicehystrixdashboard/FoodServiceHystrixDashboardApplication.java
@@ -0,0 +1,12 @@
+package pd.pduleba.spring.cloud.sl.foodservicehystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodServiceHystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodServiceHystrixDashboardApplication.class, args);
+	}
+}
---
+++ food-service-hystrix-dashboard/src/test/java/pd/pduleba/spring/cloud/sl/foodservicehystrixdashboard/FoodServiceHystrixDashboardApplicationTests.java
@@ -0,0 +1,16 @@
+package pd.pduleba.spring.cloud.sl.foodservicehystrixdashboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FoodServiceHystrixDashboardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
