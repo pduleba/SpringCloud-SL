8c691e60b81a88c6e0b0c9e75564c6e0fe865ce7
pduleba <pduleba@pgs-soft.com>
2018-05-09 12:29:02 +0200

    Chapter 5.2 :: Aggregating multiple Hystrix Dashboard :: Init

--- food-service-hystrix-dashboard/src/main/java/pd/pduleba/spring/cloud/sl/foodservicehystrixdashboard/FoodServiceHystrixDashboardApplication.java
+++ food-service-hystrix-dashboard/src/main/java/pd/pduleba/spring/cloud/sl/foodservicehystrixdashboard/FoodServiceHystrixDashboardApplication.java
@@ -2,7 +2,9 @@ package pd.pduleba.spring.cloud.sl.foodservicehystrixdashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
+@EnableHystrixDashboard
 @SpringBootApplication
 public class FoodServiceHystrixDashboardApplication {
 
