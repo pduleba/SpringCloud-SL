4a1142817e136dbcf8844aa26f59801766fa88b2
pduleba <pduleba@pgs-soft.com>
2018-05-09 13:11:40 +0200

    Chapter 5.2 :: Aggregating multiple Hystrix Dashboard :: Init Turbine

---
+++ food-service-hystrix-dashboard-turbine-aggregator/src/main/java/pd/pduleba/spring/cloud/sl/foodservicehystrixdashboardturbineaggregator/FoodServiceHystrixDashboardTurbineAggregatorApplication.java
@@ -0,0 +1,12 @@
+package pd.pduleba.spring.cloud.sl.foodservicehystrixdashboardturbineaggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodServiceHystrixDashboardTurbineAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodServiceHystrixDashboardTurbineAggregatorApplication.class, args);
+	}
+}
---
+++ food-service-hystrix-dashboard-turbine-aggregator/src/test/java/pd/pduleba/spring/cloud/sl/foodservicehystrixdashboardturbineaggregator/FoodServiceHystrixDashboardTurbineAggregatorApplicationTests.java
@@ -0,0 +1,16 @@
+package pd.pduleba.spring.cloud.sl.foodservicehystrixdashboardturbineaggregator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FoodServiceHystrixDashboardTurbineAggregatorApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
